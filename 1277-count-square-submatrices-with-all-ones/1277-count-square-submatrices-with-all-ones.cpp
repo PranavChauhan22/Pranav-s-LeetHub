b08195a210bb5087573e64af3d240a5dfa40eb1c
Pranav Chauhan <83133554+PranavChauhan22@users.noreply.github.com>
2022-04-20 13:29:03 +0530

    Time: 89 ms (52.56%), Space: 26.3 MB (23.00%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -4,24 +4,25 @@ public:
         vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),0));
         for(int i=matrix.size()-1;i>=0;i--){
             for(int j=matrix[0].size()-1;j>=0;j--){
-                if(matrix[i][j]==1){
-                    if(i==matrix.size()-1){
-                        dp[i][j]=1;
-                    }else if(j==matrix[0].size()-1){
-                        dp[i][j]=1;
-                    }else{
-                        dp[i][j]=min(dp[i+1][j+1],min(dp[i][j+1],dp[i+1][j]))+1;
+                if(i==matrix.size()-1&&j==matrix[0].size()-1&&matrix[i][j]==1){
+                    dp[i][j]=1;
+                }else if(i==matrix.size()-1&&matrix[i][j]==1){
+                    dp[i][j]=1;
+                }else if(j==matrix[0].size()-1&&matrix[i][j]==1){
+                    dp[i][j]=1;
+                }else{
+                    if(matrix[i][j]==1){
+                    dp[i][j]=min(dp[i][j+1],min(dp[i+1][j],dp[i+1][j+1]))+1;
                     }
                 }
             }
         }
         int ans=0;
-       for(int i=matrix.size()-1;i>=0;i--){
+        for(int i=matrix.size()-1;i>=0;i--){
             for(int j=matrix[0].size()-1;j>=0;j--){
                 ans+=dp[i][j];
             }
-       }
+        }
         return ans;
     }
 };
-
