7d41d0e3a1edfff52414cd3867a557da5d52c9dc
Pranav Chauhan <83133554+PranavChauhan22@users.noreply.github.com>
2022-05-29 20:53:36 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        stack<long long> st;
+        vector<long long> nge(n,-1);
+        st.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0&&st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                nge[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return nge;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
