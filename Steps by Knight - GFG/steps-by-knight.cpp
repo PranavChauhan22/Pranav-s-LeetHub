f68e583971a5e6544fac1765f8c1f9b9e762c94a
Pranav Chauhan <83133554+PranavChauhan22@users.noreply.github.com>
2022-07-01 22:28:59 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.cpp
+++ Steps by Knight - GFG/steps-by-knight.cpp
@@ -6,47 +6,49 @@ using namespace std;
 class Solution 
 {
     public:
+    
     struct Pair{
-        int x;
-        int y;
-        int l;
-        Pair(int _x,int _y, int _l){
-            x=_x;
-            y=_y;
-            l=_l;
+        int i;
+        int j;
+        int wgt;
+        Pair(int _i,int _j,int _wgt){
+            i=_i;
+            j=_j;
+            wgt=_wgt;
         }
     };
+    
     struct comp{
         bool operator()(Pair &a,Pair &b){
-            return a.l>b.l;
-        }
+            return a.wgt>b.wgt;
+        }  
     };
+    
     //Function to find out minimum steps Knight needs to reach target position.
 	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int N)
 	{
-        int arr[8][2]={{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,-1},{-2,1}};
-        priority_queue<Pair,vector<Pair>,comp>pq;
-        pq.push(Pair(KnightPos[0],KnightPos[1],0));
-        vector<vector<bool>>vis(N+1,vector<bool>(N+1,false));
-        while(pq.size()>0){
-            Pair src=pq.top();pq.pop();
-            if(src.x==TargetPos[0]&&src.y==TargetPos[1]){
-                return src.l;
-            }
-            if(vis[src.x][src.y]){
-                continue;
-            }
-            vis[src.x][src.y]=true;
-            for(int i=0;i<8;i++){
-                int xd=src.x+arr[i][0];
-                int yd=src.y+arr[i][1];
-                if(xd>=1&&yd>=1&&xd<=N&&yd<=N&&!vis[xd][yd]){
-                    pq.push(Pair(xd,yd,src.l+1));
-                }
-            }
-        }
-        return -1;
-        
+	    int arr[8][2]={{2,1},{-2,1},{-2,-1},{2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+	    vector<vector<bool>>vis(N+1,vector<bool>(N+1,false));
+	    priority_queue<Pair,vector<Pair>,comp>pq;
+	    pq.push(Pair(KnightPos[0],KnightPos[1],0));
+	    while(pq.size()>0){
+	        Pair src=pq.top();pq.pop();
+	        if(src.i==TargetPos[0]&&src.j==TargetPos[1]){
+	            return src.wgt;
+	        }
+	        if(vis[src.i][src.j]){
+	            continue;
+	        }
+	        vis[src.i][src.j]=true;
+	        for(int i=0;i<8;i++){
+	            int x=src.i+arr[i][0];
+	            int y=src.j+arr[i][1];
+	            if(x>=0&&y>=0&&x<=N&&y<=N&&!vis[x][y]){
+	                pq.push(Pair(x,y,src.wgt+1));
+	            }
+	        }
+	    }
+	    return -1;
 	}
 };
 
