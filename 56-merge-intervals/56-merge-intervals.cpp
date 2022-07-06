8faedb48e141d070283a40c8af6a5bcc618eec86
Pranav Chauhan <83133554+PranavChauhan22@users.noreply.github.com>
2022-07-06 17:47:42 +0530

    Time: 61 ms (45.59%), Space: 20.3 MB (14.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    static bool comp(vector<int> v1,vector<int> v2){
-        return v1[0]<v2[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),comp);
+    
+    vector<vector<int>> mergeSolve(vector<vector<int>>& intervals){
         stack<pair<int,int>>st;
         st.push(make_pair(intervals[0][0],intervals[0][1]));
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>st.top().second){
+            if(st.top().second<intervals[i][0]){
                 st.push(make_pair(intervals[i][0],intervals[i][1]));
             }else{
                 st.top().second=max(st.top().second,intervals[i][1]);
@@ -16,11 +13,18 @@ public:
         }
         vector<vector<int>>ans;
         while(st.size()>0){
-            vector<int> v;v.push_back(st.top().first);
+            vector<int> v;
+            v.push_back(st.top().first);
             v.push_back(st.top().second);
-            ans.push_back(v);st.pop();
+            ans.push_back(v);
+            st.pop();
         }
         reverse(ans.begin(),ans.end());
         return ans;
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        return mergeSolve(intervals);
     }
 };
